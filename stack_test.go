bbc23f2f28d12e94e419c7fb36c158668d8eafcc
El Mehdi Rami <lbiggboss19@gmail.com>
2021-04-07 01:46:51 +0100

    unit testing Stack using testify

--- stack_test.go
+++ stack_test.go
@@ -3,27 +3,26 @@ package ds
 import (
 	"fmt"
 	"testing"
-	// "github.com/stretchr/testify/assert"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestStackPush(t *testing.T) {
+	assert := assert.New(t)
 	q := Stack{}
-	if q.Size != 0 {
-		t.Errorf("queue should be empty at init time")
-	}
+	assert.Equal(0, q.Size, "queue should be empty at init time")
 
 	items := []int{1, 9, 9, 8}
 	for i, item := range items {
 		q.Push(item)
 		t.Run(fmt.Sprintf("q.Push #%d", i), func(t *testing.T) {
-			if item != q.Head.Value {
-				t.Errorf("want %d; got %d", item, q.Head.Value)
-			}
+			assert.Equal(item, q.Head.Value)
 		})
 	}
 }
 
 func TestStackPop(t *testing.T) {
+	assert := assert.New(t)
 	q := Stack{}
 
 	items := []int{1, 9, 9, 8}
@@ -34,50 +33,38 @@ func TestStackPop(t *testing.T) {
 	items = []int{8, 9, 9, 1}
 	for i, item := range items {
 		t.Run(fmt.Sprintf("q.Pop #%d", i), func(t *testing.T) {
-			top := q.Pop()
-			if item != top {
-				t.Errorf("want %d; got %d", item, top)
-			}
+			assert.Equal(item, q.Pop(), "want %d; got %d")
 		})
 	}
 
-	if q.Size != 0 {
-		t.Errorf("q.Size should be 0 after removing all queue items")
-	}
-
-	if q.Pop() != nil {
-		t.Errorf("q head should be nil after removing all queue items")
-	}
+	assert.Equal(0, q.Size, "q.Size should be 0 after removing all queue items")
+	assert.Nil(q.Pop(), "q head should be nil after removing all queue items")
 }
 
 func TestStackPeek(t *testing.T) {
+	assert := assert.New(t)
 	q := Stack{}
-	if q.Peek() != nil {
-		t.Errorf("q head should be nil at init time")
-	}
+
+	assert.Nil(q.Peek(), "q head should be nil at init time")
 
 	testFn := func(item int) {
 		q.Push(item)
-		if head := q.Peek(); head != item {
-			t.Errorf("q.Peek error: want %d; got %d", 1, head)
-		}
+		assert.Equal(item, q.Peek(), "q.Peek error: want %d; got %d", 1, q.Peek())
 	}
 	for _, item := range []int{1, 9, 9, 8} {
 		testFn(item)
 	}
 
-	if q.Size != 4 {
-		t.Errorf("want %d; got %d,", 4, q.Size)
-	}
+	assert.Equal(4, q.Size, "want %d; got %d,", 4, q.Size)
 }
 
 func TestStackInverse(t *testing.T) {
+	assert := assert.New(t)
 	q := Stack{}
+
 	testStr := "racecar"
 	for _, c := range testStr {
 		q.Push(string(c))
 	}
-	if q.ToString() != testStr {
-		t.Errorf("want %s; got %s", testStr, q.ToString())
-	}
+	assert.Equal(testStr, q.ToString(), "want %s; got %s", testStr, q.ToString())
 }
